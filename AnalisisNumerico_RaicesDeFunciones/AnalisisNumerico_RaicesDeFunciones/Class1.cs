1d158dc2fce30b74c6ebee959e28558fb7f749cd
Francisco Ambort <francrar@gmail.com>
2025-08-07 09:54:22 -0300

    .

--- AnalisisNumerico_RaicesDeFunciones/AnalisisNumerico_RaicesDeFunciones/Class1.cs
+++ AnalisisNumerico_RaicesDeFunciones/AnalisisNumerico_RaicesDeFunciones/Class1.cs
@@ -1,7 +1,9 @@
-﻿namespace AnalisisNumerico_RaicesDeFunciones
+﻿using Calculus;
+namespace AnalisisNumerico_RaicesDeFunciones
 {
     public class Class1
     {
+
 
     }
 }
--- AnalisisNumerico_RaicesDeFunciones/probando/Program.cs
+++ AnalisisNumerico_RaicesDeFunciones/probando/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculus;
+
+double fx, area;
+Calculo AnalizadorDeFunciones = new Calculo();
+
+if (AnalizadorDeFunciones.Sintaxis("2*x+2", 'x')) //pasamos la funcion con la variable a evaluar
+{
+    fx = AnalizadorDeFunciones.EvaluaFx(2.3);
+    area = AnalizadorDeFunciones.Integra(2, 5, 0.0003);
+
+    Console.WriteLine("f(2.3)={0}    Area={1}", fx, area);
+
+}
+else
+{
+    // aquí mensaje de error en sintaxis
+}
