f242da4fde41c093b90b9f5f2894c1d3235b48f4
FranzHeinen <166876460+FranzHeinen@users.noreply.github.com>
2025-08-07 11:11:42 -0300

    Método Regla Falsa

--- AnalisisNumerico_RaicesDeFunciones/AnalisisNumerico_RaicesDeFunciones/MetodosCerrados.cs
+++ AnalisisNumerico_RaicesDeFunciones/AnalisisNumerico_RaicesDeFunciones/MetodosCerrados.cs
@@ -44,7 +44,7 @@ namespace AnalisisNumerico_RaicesDeFunciones
             if (fxi * fxd == 0)
             {
                 result.Iteraciones = 1;
-                result.Error = 1;
+                result.Error = 0; // Preguntar profe
                 result.Converge = true;
                 if (fxi == 0)
                 {
@@ -76,6 +76,95 @@ namespace AnalisisNumerico_RaicesDeFunciones
                     else
                     {
                         if (calculo.EvaluaFx(xi)*fxr < 0)
+                        {
+                            xi = xr;
+                        }
+                        else
+                        {
+                            xd = xr;
+                        }
+                        xrAnterior = xr;
+                    }
+                }
+                result.Xr = xr;
+                result.Iteraciones = maxiteraciones;
+                result.Error = error;
+                result.Converge = false;
+                return result;
+            }
+        }
+        public static MetodoCerradoResultado ReglaFalsa(string fx, int maxiteraciones, double tolerancia)
+        {
+            var result = new MetodoCerradoResultado { Metodo = "Regla Falsa" };
+            Calculo calculo = new Calculo();
+
+            while (!calculo.Sintaxis(fx, 'x'))
+            {
+                Console.WriteLine("Error en la sintaxis de la función. Ingresá de nuevo:");
+                fx = Console.ReadLine();
+            }
+
+            result.Funcion = fx;
+
+            double xi, xd, fxi, fxd;
+
+            while (true)
+            {
+                Console.Write("Ingresá xi: ");
+                xi = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingresá xd: ");
+                xd = double.Parse(Console.ReadLine());
+
+                fxi = calculo.EvaluaFx(xi);
+                fxd = calculo.EvaluaFx(xd);
+
+                if (fxi * fxd < 0)
+                    break;
+
+                Console.WriteLine("No hay cambio de signo. Probá con otro intervalo.\n");
+            }
+
+            if (fxi * fxd == 0)
+            {
+                result.Iteraciones = 1;
+                result.Error = 0; // Preguntar profe
+                result.Converge = true;
+                if (fxi == 0)
+                {
+                    result.Xr = xi;
+                }
+                else
+                {
+                    result.Xr = xd;
+                }
+                return result;
+            }
+            else
+            {
+                double xrAnterior = xi; //pregutar a profe
+                double xr = 0;
+                double error = 0;
+
+                for (int i = 1; i < maxiteraciones; i++)
+                {
+                    xr = (xi * fxd - xd * fxi) / (fxd - fxi);
+
+                    error = Math.Abs((xr - xrAnterior) / xr);
+
+                    double fxr = calculo.EvaluaFx(xr);
+
+                    if (Math.Abs(fxr) < tolerancia || error < tolerancia)
+                    {
+                        result.Xr = xr;
+                        result.Iteraciones = i;
+                        result.Error = error;
+                        result.Converge = true;
+                        return result;
+                    }
+                    else
+                    {
+                        if (calculo.EvaluaFx(xi) * fxr < 0)
                         {
                             xi = xr;
                         }
